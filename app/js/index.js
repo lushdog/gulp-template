3587c2bffee7d5deded62cf86121a700021ac160
impressioncr <impressioncr@hotmail.com>
2017-08-06 15:23:53 +0800

    Signed-off-by: impressioncr <impressioncr@hotmail.com>

---
+++ app/js/index.js
@@ -0,0 +1,9 @@
+class Test{
+  constructor() {
+    this.a = 'hello es6'
+  }
+}
+
+let test = new Test()
+
+document.body.innerHTML = test.a
---
+++ tasks/util/args.js
@@ -0,0 +1,38 @@
+import yargs from 'yargs'
+
+const args = yargs
+
+  .option('production', {
+    boolean: true,
+    default: false,
+    describe: 'min all script',
+  })
+
+  .option('watch', {
+    boolean: true,
+    default: false,
+    describe: 'min all script',
+  })
+
+  .option('verbose', {
+    boolean: true,
+    default: false,
+    describe: 'log',
+  })
+
+  .option('sourcemaps', {
+    describe: 'force the creation of sourcemaps',
+  })
+
+  .option('port', {
+    string: true,
+    default: 8080,
+    describe: 'server port',
+  })
+
+  .argv
+
+  export default args
+
+
+
